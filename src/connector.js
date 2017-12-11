bdac973b036a586fb00f9ff5d08789f4fd68dd13
李茂峰 <limaofeng@msn.com>
2017-12-11 14:34:18 +0800

    add connector

---
+++ src/connector.js
@@ -0,0 +1,25 @@
+import { merge, map, union, without, castArray } from 'lodash';
+
+const combine = (features, extractor) => without(union(...map(features, res => castArray(extractor(res)))), undefined);
+
+export default class {
+  // eslint-disable-next-line no-unused-vars
+  constructor({ route, navItem, reducer }, ...features) {
+    this.tabItem = combine(arguments, arg => arg.tabItem);
+    this.reducer = combine(arguments, arg => arg.reducer);
+    this.route = combine(arguments, arg => arg.route);
+  }
+
+  get tabItems() {
+    return merge(...this.tabItem);
+  }
+
+  get reducers() {
+    return merge(...this.reducer);
+  }
+
+  get routes() {
+    return this.route.map((component, idx) => React.cloneElement(component, { key: idx + this.route.length }));
+  }
+
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { createAppNavigator as createAppNavigatorTemp } from './navigation'
+import connector from './connector';
+
+export const createAppNavigator = createAppNavigatorTemp;
+export const Feature = connector;
