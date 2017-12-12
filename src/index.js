430d442573ab9becf4977ed6c34e33b620d93584
李茂峰 <limaofeng@msn.com>
2017-12-12 16:30:41 +0800

    Redux Integration

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import Entypo from 'react-native-vector-icons/Entypo';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
+import { createAppNavigator as createAppNavigatorTemp } from './navigation'
+import connector from './connector';
+
 export const loadFonts = () => {
   Entypo.loadFont();
   FontAwesome.loadFont();
   Ionicons.loadFont();
 };
+
+export const Feature = connector;
+export const createAppNavigator = createAppNavigatorTemp
