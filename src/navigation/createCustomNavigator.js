b38445082e984c1ca1b41f3db24d623e4e3fb498
李茂峰 <limaofeng@msn.com>
2017-12-22 15:26:08 +0800

    optimize code

--- src/navigation/createCustomNavigator.js
+++ src/navigation/createCustomNavigator.js
@@ -1,30 +1,4 @@
 import { createNavigationContainer, createNavigator, StackRouter } from 'react-navigation';
 
-function randomString(len = 8) {
-  const $chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';
-  const maxPos = $chars.length;
-  let pwd = '';
-  for (i = 0; i < len; i++) {
-    pwd += $chars.charAt(Math.floor(Math.random() * maxPos));
-  }
-  return pwd;
-}
-
-export default transitioner => (main, right) => {
-  const routes = {};
-  const keyMain = `main-${randomString(8)}`;
-  const keyRight = `right-${randomString(8)}`;
-  routes[keyMain] = {
-    screen: main,
-    navigationOptions: {
-      reverseRouteName: keyRight
-    }
-  };
-  routes[keyRight] = {
-    screen: right
-  };
-  const config = {
-    initialRouteName: keyMain
-  };
-  return createNavigationContainer(createNavigator(StackRouter(routes, config))(transitioner));
-};
+export default transitioner => (routes, config) =>
+  createNavigationContainer(createNavigator(StackRouter(routes, config))(transitioner));
