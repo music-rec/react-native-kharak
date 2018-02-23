d2376fdd05974cd5a0971b39a8f4b857dd3f701c
李茂峰 <limaofeng@msn.com>
2018-02-24 00:19:48 +0800

    connector

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
-import Entypo from 'react-native-vector-icons/Entypo';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-
-import { NotificationProvider } from './notification';
+// import { NotificationProvider } from './notification';
 import Feature from './connector';
 
-export const loadFonts = () => {
-  Entypo.loadFont();
-  FontAwesome.loadFont();
-  Ionicons.loadFont();
-};
-
-export * from './navigation';
-export * from './redux';
-export { Feature, NotificationProvider };
+// export * from './navigation';
+// export * from './redux';
+export { Feature };
